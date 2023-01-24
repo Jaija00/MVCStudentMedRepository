bdf742c3570648ba60d98c11b7f699fe56f8386d
Robin Stenlund <robin.stenlund@gmail.com>
2023-01-24 13:38:31 +0100

    ViewModel for index added

---
+++ MVCStudentMedRepository/ViewModels/StudentCourseIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace MVCStudentMedRepository.ViewModels
+{
+    public class StudentCourseIndexViewModel
+    {
+        public int Id { get; set; }
+        public string Student { get; set; }
+        public string Course { get; set; }
+        public string Grade { get; set; } = "";
+        public bool Completed { get; set; }
+    }
+}
